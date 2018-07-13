196eb996ec18fcc47f462423a0516df1cf258e34
TonyKioko <tonykioko384@gmail.com>
2018-07-13 15:56:18 +0300

    backend and front end logic working

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
 
-// USER INTERFACE LOGIC
-$(document).ready(function(){
-  $("form#ping-form").submit(function(event){
-    event.preventDefault();
-    var numInput = parseInt($("input#number").val());
-     var result = pingPong(numInput);
-     console.log(result);
-    // alert(numInput);
-  })
-})
+/** --- BACKEND LOGIC --**/
 
+var pingPong = function (i) {
 
-
-
-// BUSINESS LOGIC
-var pingPong = function(num){
-   for (var i =0; i <=num; i++){
-     if (num %15 === 0){
-       return "pingpong"
-     } else if (num %3 === 0){
-       return "ping"
-     } else if (num %5 === 0){
-       return "pong"
-     } else {
-       return num
-     }
-   }
+	if (i % 15 ===0) {
+		return "pingpong"
+		}
+	else if(i% 3===0){
+		return "ping"
+		}
+	else if(i%5===0){
+		return "pong"
+		}
+	else{
+		return i
+		}
 }
+
+/** --- USER INTERFACE LOGIC --**/
+$(document).ready(function(){
+	$("form#ping-form").submit(function(event){
+		event.preventDefault();
+    // alert("submitted")
+		$("ul.ping-list").empty();
+		var number=parseInt($("input#number").val());
+    alert(number)
+		for(var i=1; i<=number; i++){
+			$("ul.ping-list").append("<li>"+pingPong(i)+"</li>");
+		}
+	});
+});
