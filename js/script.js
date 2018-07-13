a30086805f1c7c59056b8c938c2b6b8bccf601d8
TonyKioko <tonykioko384@gmail.com>
2018-07-13 12:33:06 +0300

    form input capture success

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 
+// USER INTERFACE LOGIC
+$(document).ready(function(){
+  $("form#ping-form").submit(function(event){
+    event.preventDefault();
+    var numInput = parseInt($("input#number").val());
+     var result = pingPong(numInput);
+     console.log(result);
+    // alert(numInput);
+  })
+})
+
+
+
+
+// BUSINESS LOGIC
+var pingPong = function(num){
+   for (var i =0; i <=num; i++){
+     if (num %15 === 0){
+       return "pingpong"
+     } else if (num %3 === 0){
+       return "ping"
+     } else if (num %5 === 0){
+       return "pong"
+     } else {
+       return num
+     }
+   }
+}
